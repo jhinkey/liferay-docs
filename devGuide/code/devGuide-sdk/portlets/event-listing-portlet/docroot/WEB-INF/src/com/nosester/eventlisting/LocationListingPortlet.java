a62c6e7658f2f637869261a6c76478d477b41dba
Jim Hinkey <james.hinkey@liferay.com>
2013-12-19 16:41:12 -0500

    LRDOCS-873 Create locationlisting portlet plugin.

---
+++ devGuide/code/devGuide-sdk/portlets/event-listing-portlet/docroot/WEB-INF/src/com/nosester/eventlisting/LocationListingPortlet.java
@@ -0,0 +1,11 @@
+package com.nosester.eventlisting;
+
+import com.liferay.util.bridges.mvc.MVCPortlet;
+
+/**
+ * Portlet implementation class LocationListingPortlet
+ */
+public class LocationListingPortlet extends MVCPortlet {
+ 
+
+}
